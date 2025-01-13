859265c0ae20807731f86c040b38a0dccb83eecc
koradiyayash26 <yashkoradiya26@gmail.com>
2025-01-13 15:49:17 +0530

    fix some

--- conf/settings.py
+++ conf/settings.py
@@ -196,13 +196,13 @@ DATABASES = {
     }
 }
 
-if DEBUG:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-else:
-    DATABASES['default'].update(dj_database_url.config(
+# if DEBUG:
+#     DATABASES['default'] = {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# else:
+DATABASES['default'].update(dj_database_url.config(
         default=config('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
