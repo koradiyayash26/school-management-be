0fcc68283235c30e7ddf3de0ce84ee7b89417308
koradiyayash26 <yashkoradiya26@gmail.com>
2025-01-16 12:17:14 +0530

    Update settings.py to add new CORS allowed origins for frontend applications. Included 'https://school-management-fe.onrender.com' and 'https://school-management-fe.onrender.com/login' to enhance cross-origin resource sharing for the application.

--- conf/settings.py
+++ conf/settings.py
@@ -87,7 +87,8 @@ CORS_ALLOW_CREDENTIALS = True
 
 SOCKETIO_CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',  # Your frontend URL
-    'https://school-management-fe.vercel.app'
+    'https://school-management-fe.vercel.app',
+    'https://school-management-fe.onrender.com'
 ]
 
 
@@ -164,6 +165,7 @@ ROOT_URLCONF = 'conf.urls'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',  # Adjust this to match your React.js app's URL
     'https://school-management-fe.vercel.app',
+    'https://school-management-fe.onrender.com/login',
     "https://school-management-fe-git-main-yashs-projects-5692f090.vercel.app",
     "https://school-management-75vghc1ge-yashs-projects-5692f090.vercel.app"
 ]
