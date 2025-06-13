818c25b5a5f66d4844ae7e0290295f489e125531
koradiyayash26 <yashkoradiya26@gmail.com>
2025-06-13 14:39:32 +0530

    fix database

--- conf/settings.py
+++ conf/settings.py
@@ -216,24 +216,24 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 # }
 
 
+ENVIRONMENT = config('ENVIRONMENT', default='development')
 
-if os.name == 'nt':
+# Database configuration
+if ENVIRONMENT == 'development':
     DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': BASE_DIR / 'db.sqlite3',
-            }
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
-# else:
-#     DATABASES['default'].update(dj_database_url.config(
-#             default=config('DATABASE_URL'),
-#             conn_max_age=600,
-#             ssl_require=True
-#         ))
-if not os.name == "nt":
-    DATABASES['default']= dj_database_url.parse("postgresql://school_database_1g40_user:cMZQloH4uf8yAvEQl0caS4BU2Aseoelw@dpg-cu3rhml2ng1s73cdqeog-a.oregon-postgres.render.com/school_database_1g40")
-
-
+    }
+else:  # Production database
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
